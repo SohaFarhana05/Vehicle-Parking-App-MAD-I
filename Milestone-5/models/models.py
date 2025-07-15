1589918aebd70639c3cd42e8ea7362b2af5cb7f1
24f1001758 <24f1001758@ds.study.iitm.ac.in>
2025-07-15 17:50:11 +0530

    Milestone-VP Summary-Users-Admin

---
+++ Milestone-5/models/models.py
@@ -0,0 +1,55 @@
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Admin(db.Model):
+    __tablename__ = 'admins'
+    id = db.Column(db.Integer , primary_key = True)
+    username = db.Column(db.String(20) , unique=True , nullable=False)
+    password =db.Column(db.String(255),nullable =False)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),unique=True, nullable=False)
+    password = db.Column(db.String(255),nullable=False)
+    full_name = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    reservations = db.relationship('Reservation', backref='user', cascade='all, delete-orphan')
+    
+    def get_current_reservation(self):
+        """Get the current active reservation for this user"""
+        return Reservation.query.filter_by(
+            user_id=self.id, 
+            leaving_timestamp=None
+        ).first()
+
+class ParkingLot(db.Model):
+    __tablename__ = 'parking_lots'
+    id = db.Column(db.Integer, primary_key=True)
+    prime_location_name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    pin_code = db.Column(db.String(10), nullable=False)
+    maximum_number_of_spots = db.Column(db.Integer, nullable=False)
+    spots = db.relationship('ParkingSpot', backref='lot', cascade='all, delete-orphan')
+
+class ParkingSpot(db.Model):
+    __tablename__ = 'parking_spots'
+    id = db.Column(db.Integer, primary_key=True)
+    lot_id = db.Column(db.Integer, db.ForeignKey('parking_lots.id'), nullable=False)
+    status = db.Column(db.String(1), default='A')  # A for Available, R for Reserved, O for Occupied
+
+class Reservation(db.Model):
+    __tablename__ = 'reservations'
+    id = db.Column(db.Integer, primary_key=True)
+    spot_id = db.Column(db.Integer, db.ForeignKey('parking_spots.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    reservation_timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    parking_timestamp = db.Column(db.DateTime, nullable=True)
+    leaving_timestamp = db.Column(db.DateTime, nullable=True)
+    parking_cost = db.Column(db.Float, nullable=True)
+
+    spot = db.relationship('ParkingSpot', backref='reservations')
